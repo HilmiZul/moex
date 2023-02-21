8f1e1839be75803fbf4194ae9e9bcffb8a956c80
hilmizul <netspytux@gmail.com>
2023-02-21 14:33:56 +0700

    first commit

---
+++ obstacle.js
@@ -0,0 +1,31 @@
+class Obstacle {
+  constructor() {
+    this.pos = createVector(
+                random(50, width), random(50, height/2))
+    this.r = 50
+  }
+  
+  reset() {
+    this.pos = createVector(
+                random(50, width), random(50, height/2))
+  }
+  
+  show() {
+    noStroke()
+    ellipse(this.pos.x, this.pos.y, this.r)
+  }
+  
+  hitLeft(lw, i) {
+    let hit = collideCircleCircle(
+      lw.x, lw.y, 20,
+      obstacles[i].pos.x, obstacles[i].pos.y, this.r)
+    return hit
+  }
+  
+  hitRight(rw, i) {
+    let hit = collideCircleCircle(
+      rw.x, rw.y, 20, 
+      obstacles[i].pos.x, obstacles[i].pos.y, this.r)
+    return hit
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,98 @@
+let video;
+let poseNet;
+let poses = [];
+let pose;
+let leftWrist;
+let rightWrist;
+
+let rWrist = 30
+let obstacles = [];
+let nObs = 2;
+let hit = false;
+let skorKuning = 0;
+let skorMerah = 0;
+
+function setup() {
+  createCanvas(900, 640);
+
+  for (let i = 0; i < nObs; i++) {
+    obstacles.push(new Obstacle());
+  }
+
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on("pose", function (results) {
+    poses = results;
+  });
+
+  video.hide();
+  // poseNet.flipHorizontal = true
+}
+
+function modelReady() {
+  select("#status").html("Model Loaded");
+}
+
+function draw() {
+  translate(width,0);
+  scale(-1,1); // flip video biar ga susah mainnya
+  image(video, 0, 0, width, height);
+  strokeWeight(2);
+
+  push();
+  translate(width,0)
+  scale(-1,1)
+  fill(213, 0, 143);
+  stroke(0);
+  textSize(50);
+  text(skorKuning, 10, 50);
+  pop();
+
+  push();
+  translate(width,0)
+  scale(-1,1)
+  fill(255, 215, 0);
+  stroke(0);
+  textSize(50);
+  text(skorMerah, width - 50, 50);
+  pop();
+
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+
+    fill(213, 0, 143);
+    leftWrist = pose.leftWrist;
+    ellipse(leftWrist.x, leftWrist.y, rWrist);
+
+    fill(255, 215, 0);
+    rightWrist = pose.rightWrist;
+    ellipse(rightWrist.x, rightWrist.y, rWrist);
+
+    for (let i = 0; i < nObs; i++) {
+      i == 0 ? fill(213, 0, 143) : fill(255, 215, 0);
+      obstacles[i].show();
+      if (obstacles[i].hitLeft(leftWrist, i)) {
+        obstacles.splice(i, 1);
+        skorKuning++;
+        obstacles.push(new Obstacle());
+      }
+      if (obstacles[i].hitRight(rightWrist, i)) {
+        obstacles.splice(i, 1);
+        skorMerah++;
+        obstacles.push(new Obstacle());
+      }
+    }
+  }
+}
+
+
+function keyPressed() {
+  if(key === 'r') {
+    skorKuning = 0
+    skorMerah = 0
+  }
+}
+
+
