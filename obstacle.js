d23111a9e3163d608fb2dee5aa198e33c985acf9
hilmizul <netspytux@gmail.com>
2025-04-05 22:28:03 +0700

    update: collision method params

--- obstacle.js
+++ obstacle.js
@@ -1,31 +1,29 @@
 class Obstacle {
   constructor() {
     this.pos = createVector(
-                random(50, width), random(50, height/2))
+      random(50, width),
+      random(50,height / 2)
+    )
     this.r = 60
   }
-  
+
   reset() {
     this.pos = createVector(
-                random(50, width), random(50, height/2))
+      random(50, width),
+      random(50, height / 2)
+    )
   }
-  
+
   show() {
     noStroke()
     ellipse(this.pos.x, this.pos.y, this.r)
   }
-  
-  hitLeft(lw, i) {
+
+  collisionWrist(wristX, wristY, i) {
     let hit = collideCircleCircle(
-      lw.x, lw.y, 20,
-      obstacles[i].pos.x, obstacles[i].pos.y, this.r)
-    return hit
-  }
-  
-  hitRight(rw, i) {
-    let hit = collideCircleCircle(
-      rw.x, rw.y, 20, 
-      obstacles[i].pos.x, obstacles[i].pos.y, this.r)
+      wristX, wristY, 20,
+      obstacles[i].pos.x, obstacles[i].pos.y, this.r
+    )
     return hit
   }
 }
