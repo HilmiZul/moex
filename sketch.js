26cdcf990a8d93494cae39f9efd5ca1bab615a82
hilmizul <netspytux@gmail.com>
2025-04-05 22:28:38 +0700

    improvement (and refactoring)

--- sketch.js
+++ sketch.js
@@ -1,41 +1,42 @@
+// Created at: 16 December 2022, Bandara Kertajati, Majalengka
+// Updated at: 5 April 2025
+// Created by: Zul Hilmi
+// Repo: hilmizul/moex
+
 let video;
-let poseNet;
-let poses = [];
+let handPose;
+let hands = [];
 let pose;
 let leftWrist;
 let rightWrist;
-
-let rWrist = 50
+let radiusWrist = 50
 let obstacles = [];
-let nObs = 2;
-let hit = false;
+let n_obs = 2;
 let skorKuning = 0;
 let skorMerah = 0;
 
 let timer = 120
 
-function setup() {
-  createCanvas(1360, 900);
+function preload() {
+  handPose = ml5.handPose({ flipped: false });
+}
 
-  for (let i = 0; i < nObs; i++) {
+function setup() {
+  createCanvas(960, 720);
+
+  for (let i = 0; i < n_obs; i++) {
     obstacles.push(new Obstacle());
   }
 
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO, { flipped: false });
   video.size(width, height);
-
-  poseNet = ml5.poseNet(video, modelReady);
-  poseNet.on("pose", function (results) {
-    poses = results;
-  });
-
   video.hide();
-  // poseNet.flipHorizontal = true
-  // frameRate(1)
+  handPose.detectStart(video, modelReady);
 }
 
-function modelReady() {
-  select("#status").html("<h2>MORNING EXERCISE</h2>");
+function modelReady(results) {
+  hands = results;
+  select("#status").html("<h2>🕺🏻 MORNING EXERCISE 💃🏻</h2>");
 }
 
 function playAgain() {
@@ -75,6 +76,7 @@ function draw() {
   pop()
   // end timer
 
+  // score
   push();
   translate(width - 20, 0)
   scale(-1, 1)
@@ -92,35 +94,47 @@ function draw() {
   textSize(50);
   text(skorMerah, width - 50, 50);
   pop();
+  // end score
 
-  if (poses.length > 0) {
-    pose = poses[0].pose;
+  // deteksi titik pergelangan tangan dari handPose
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    for (let j = 0; j < hand.keypoints.length; j++) {
+      if(hand.handedness == "Left") {
+        leftWrist = hand.keypoints[0];
+        fill(213, 0, 143);
+        stroke(0);
+        strokeWeight(5);
+        circle(leftWrist.x, leftWrist.y, radiusWrist);
+      } else if(hand.handedness == "Right") {
+        rightWrist = hand.keypoints[0];
+        fill(255, 215, 0);
+        stroke(0);
+        strokeWeight(5)
+        circle(rightWrist.x, rightWrist.y, radiusWrist);
+      }
+    }
+  }
 
-    fill(213, 0, 143);
-    leftWrist = pose.leftWrist;
-    strokeWeight(5)
-    stroke(0)
-    ellipse(leftWrist.x, leftWrist.y, rWrist);
-    // rect(width-50, leftWrist.y, 50, 100);
-
-
-    fill(255, 215, 0);
-    rightWrist = pose.rightWrist;
-    ellipse(rightWrist.x, rightWrist.y, rWrist);
-    // rect(0, rightWrist.y, 50, 100);
-
-    for (let i = 0; i < nObs; i++) {
+  // draw obstacles saat tangan muncul.
+  // cek collision masing-masing tangan dengan obstacles.
+  if(hands.length > 0) {
+    for (let i = 0; i < n_obs; i++) {
       i == 0 ? fill(213, 0, 143) : fill(255, 215, 0);
       obstacles[i].show();
-      if (obstacles[i].hitLeft(leftWrist, i)) {
-        obstacles.splice(i, 1);
-        skorKuning++;
-        obstacles.push(new Obstacle());
+      if(rightWrist) {
+        if (obstacles[i].collisionWrist(rightWrist.x, rightWrist.y, i)) {
+          obstacles.splice(i, 1);
+          skorMerah++;
+          obstacles.push(new Obstacle());
+        }
       }
-      if (obstacles[i].hitRight(rightWrist, i)) {
-        obstacles.splice(i, 1);
-        skorMerah++;
-        obstacles.push(new Obstacle());
+      if(leftWrist) {
+        if (obstacles[i].collisionWrist(leftWrist.x, leftWrist.y, i)) {
+          obstacles.splice(i, 1);
+          skorKuning++;
+          obstacles.push(new Obstacle());
+        }
       }
     }
   }
@@ -128,14 +142,17 @@ function draw() {
 
 
 function keyPressed() {
+  // reset saat gameplay
   if (key === 'r') {
     skorKuning = 0
     skorMerah = 0
+    obstacles[0].reset()
+    obstacles[1].reset()
+    playAgain()
   }
   if (key === 'p') {
+    // play again saat game berakhir
     playAgain()
     loop()
   }
 }
-
-
